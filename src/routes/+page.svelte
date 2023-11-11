c63e7e1118055b2cbe15731f030fd38c213474f5
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-11-11 16:01:04 +0900

    あ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,16 +11,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
+	<h1>Login With Google.</h1>
 
 	<h2>
 		try editing <strong>src/routes/+page.svelte</strong>
