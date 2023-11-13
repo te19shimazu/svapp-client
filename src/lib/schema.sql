18d452a1de80d76ca508bf17e7f830ef8d59c966
te19shimazu <keiichi.cuber@gmail.com>
2023-11-13 15:10:03 +0900

    id名の変更とauto_increment

--- src/lib/schema.sql
+++ src/lib/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Employees (
-    user_id INT PRIMARY KEY,
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100),
     email VARCHAR(255),
     role VARCHAR(100)
@@ -7,24 +7,24 @@ CREATE TABLE Employees (
 
 CREATE TABLE AttendanceRecords (
     record_id INT PRIMARY KEY,
-    employee_id INT,
+    user_id INT,
     date DATE,
     time_in TIME,
     time_out TIME,
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE WorkTimeManagement (
     record_id INT PRIMARY KEY,
-    employee_id INT,
+    user_id INT,
     date DATE,
     worked_hours DECIMAL(5, 2),
     overtime_hours DECIMAL(5, 2),
     break_time DECIMAL(5, 2),
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE INDEX idx_employee_id ON Employees(employee_id);
+CREATE INDEX idx_user_id ON Users(user_id);
 
-CREATE INDEX idx_worktime_employee_id ON WorkTimeManagement(employee_id);
+CREATE INDEX idx_worktime_user_id ON WorkTimeManagement(user_id);
 CREATE INDEX idx_worktime_date ON WorkTimeManagement(date);
