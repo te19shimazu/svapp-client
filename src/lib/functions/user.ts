0138a52fec9a09a63b73d4304b00d00489542b7a
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-11-14 10:52:00 +0900

    Fix getUserByEmail function to fetch user data by email

--- src/lib/functions/user.ts
+++ src/lib/functions/user.ts
@@ -4,7 +4,7 @@ export async function getUserByEmail(email: string|null): Promise<User> {
   if (!email) {
     return null as unknown as User;
   }
-  const response = await fetch(`/api/users/${email}`);
+  const response = await fetch(`/api/users/?email=${email}`);
   const userData = await response.json();
   return userData;
 }
