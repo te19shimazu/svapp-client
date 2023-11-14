1815c06ffbe26769f5726e0df26b91c24fdbe16e
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-11-14 12:07:26 +0900

    Fix API endpoint URL in getUserByEmail function.

--- src/lib/functions/user.ts
+++ src/lib/functions/user.ts
@@ -4,7 +4,7 @@ export async function getUserByEmail(email: string | null): Promise<User> {
   if (!email) {
     return null as unknown as User;
   }
-  const response = await fetch(`svapp-server.hinaharu-0014.workers.dev/api/users/${encodeURIComponent(email)}`);
+  const response = await fetch(`https://svapp-server.hinaharu-0014.workers.dev/api/users/${encodeURIComponent(email)}`);
   const userData = await response.json();
   return userData;
 }
