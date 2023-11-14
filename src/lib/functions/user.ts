87e86400b50476acdc8f3785d17829f61f8f60d7
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-11-14 12:02:42 +0900

    Fix fetch URL in getUserByEmail function.

--- src/lib/functions/user.ts
+++ src/lib/functions/user.ts
@@ -1,10 +1,10 @@
 import type { User } from "firebase/auth";
 
-export async function getUserByEmail(email: string|null): Promise<User> {
+export async function getUserByEmail(email: string | null): Promise<User> {
   if (!email) {
     return null as unknown as User;
   }
-  const response = await fetch(`/api/users?email=${email}`);
+  const response = await fetch(`svapp-server.hinaharu-0014.workers.dev/api/users/${encodeURIComponent(email)}`);
   const userData = await response.json();
   return userData;
 }
