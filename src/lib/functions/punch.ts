0e449ca5cfcc5765c23d86fa27008df27f811b5e
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-12-12 00:12:33 +0900

    Fix body format in sendPunchRequest function

--- src/lib/functions/punch.ts
+++ src/lib/functions/punch.ts
@@ -1,7 +1,7 @@
 
 export async function sendPunchRequest(sessionId: string): Promise<{ ok: boolean, now: string, status: string }> {
   const url = 'https://svapp-server.hinaharu-0014.workers.dev/api/punch';
-  const body = { sessionId };
+  const body = { "sessionId": sessionId };
   const response = await fetch(url, {
     method: 'POST',
     headers: {
@@ -10,16 +10,13 @@ export async function sendPunchRequest(sessionId: string): Promise<{ ok: boolean
     body: JSON.stringify(body)
   });
   const c = await response.json();
-  try {
-    if (!response.ok) {
-      throw new Error(c.message);
-    }else if (c.time_in) {
-      return { ok: true, now: c.time_in, status: '出勤' };
-    }else if (c.time_out) {
-      return { ok: true, now: c.time_out, status: '退勤' };
-    }
-  }catch (e) {
+  if (!response.ok) {
+    throw new Error(c.message);
+  } else if (c.time_in) {
+    return { ok: true, now: c.time_in, status: '出勤' };
+  } else if (c.time_out) {
+    return { ok: true, now: c.time_out, status: '退勤' };
+  } else {
     return { ok: false, now: c.message, status: 'エラー' };
   }
-  return { ok: false, now: c.message, status: 'エラー' };
 }
