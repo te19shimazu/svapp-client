88da5998674744423610cea12215ea2838b110c0
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-12-13 20:26:41 +0900

    Refactor page.svelte component to include salary information

--- src/routes/records/+page.svelte
+++ src/routes/records/+page.svelte
@@ -4,37 +4,41 @@
 	import { derived } from 'svelte/store';
 	import { getRecords } from '$lib/functions/record';
 
-  let attendanceHistory = [];
-  let records = [];
-  let pageNumber;
+	let attendanceHistory = [];
+	let records = [];
+	let pageNumber;
+	let salary;
 
-  // ページ番号を取得するための派生ストア
-  const pageNumberStore = derived(page, $page => {
-    const queryParam = $page.url.searchParams.get('page');
-    return queryParam ? parseInt(queryParam, 10) : 1;
-  });
+	// ページ番号を取得するための派生ストア
+	const pageNumberStore = derived(page, ($page) => {
+		const queryParam = $page.url.searchParams.get('page');
+		return queryParam ? parseInt(queryParam, 10) : 1;
+	});
 
-  // pageNumberStoreを購読して、ページ番号が変更されたときにレコードを取得
-  pageNumberStore.subscribe(async (value) => {
-    pageNumber = value;
-    
-    // セッションIDを取得
-    const sessionId = sessionStorage.getItem('sessionId');
-    if (!sessionId) {
-      alert('セッションIDがありません');
-      return;
-    }
+	// pageNumberStoreを購読して、ページ番号が変更されたときにレコードを取得
+	pageNumberStore.subscribe(async (value) => {
+		pageNumber = value;
 
-    // getRecordsを呼び出してレコードを取得
-    const response = await getRecords(sessionId, pageNumber);
-    attendanceHistory = await response.json();
-    records = attendanceHistory.records.results;
-  });
-  
+		// セッションIDを取得
+		const sessionId = sessionStorage.getItem('sessionId');
+		if (!sessionId) {
+			alert('セッションIDがありません');
+			return;
+		}
+
+		// getRecordsを呼び出してレコードを取得
+		const response = await getRecords(sessionId, pageNumber);
+		attendanceHistory = await response.json();
+		records = attendanceHistory.records.results;
+		salary = attendanceHistory.salary;
+	});
 </script>
 
 <main>
 	<h1>勤怠履歴</h1>
+	{#if salary}
+		<p>あなたの時給は{salary}円です。</p>
+	{/if}
 	<table>
 		<thead>
 			<tr>
@@ -46,21 +50,21 @@
 		<tbody>
 			{#each records as record}
 				<tr>
-          <td>{record.date}</td>
+					<td>{record.date}</td>
 					<td>{record.time_in}</td>
 					<td>{record.time_out}</td>
 				</tr>
 			{/each}
 		</tbody>
 	</table>
-  <div>
-    {#if pageNumber > 1}
-      <a href="/records?page={pageNumber - 1}">前へ</a>
-    {/if}
-  </div>
-  <div>
-    <a href="/records?page={pageNumber + 1}">次へ</a>
-  </div>
+	<div>
+		{#if pageNumber > 1}
+			<a href="/records?page={pageNumber - 1}">前へ</a>
+		{/if}
+	</div>
+	<div>
+		<a href="/records?page={pageNumber + 1}">次へ</a>
+	</div>
 </main>
 
 <style>
