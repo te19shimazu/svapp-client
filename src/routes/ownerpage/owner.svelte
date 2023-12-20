79179582cf95d877913d5699dafa8c8a76da1b19
te19shimazu <te19shimazu@g.kumamoto-nct.ac.jp>
2023-12-20 16:05:33 +0900

    form作った

--- src/routes/ownerpage/owner.svelte
+++ src/routes/ownerpage/owner.svelte
@@ -1,21 +1,47 @@
 <script>
-    import { goto } from '$app/navigation';
-	import { getUserFromSession } from '$lib/functions/user';
-	import { auth } from '../about/firebase';
-	import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { getUserFromSession } from '$lib/functions/user';
+  import { auth } from '../about/firebase';
+  import { onMount } from 'svelte';
 
-    function handleLogout() {
+  function handleLogout() {
 		sessionStorage.clear();
 		auth.signOut();
 		goto('/');
 	}
 	function handleAdd(){
-		console.log('User add');
+		goto('./mypage');
 	}
 </script>
 <center>
 <h1>congratulation!!!</h1>
 <h3>Login successed!</h3>
 </center>
-<button type="button" on:click={handleLogout}> ログアウト </button>
-<button type="button" on:click={handleAdd}>ユーザー追加</button>
+<form action="" method="post" name="add">
+
+    <label for="name">名前:</label>
+    <input type="text" id="name" name="name" required>
+
+    <br>
+
+    <label for="email">メールアドレス:</label>
+    <input type="email" id="email" name="email" required>
+
+    <br>
+
+    <label for="position">役職:</label>
+    <select id="position" name="position" required>
+      <option value="" disabled selected>選択してください</option>
+      <option value="owner">管理者</option>
+	    <option value="maneger">マネージャー</option>
+	    <option value="parttime">パート</option>
+	    <option value="others">一般人</option>
+    </select>
+
+    <br>
+
+    <button type="submit">従業員登録</button>
+
+  </form>
+
+  <button type="button" on:click={handleLogout}> ログアウト </button>
