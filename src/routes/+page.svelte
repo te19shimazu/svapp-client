01da8774f098b4bc4c00ad35fa21722b3ca98cad
te19shimazu <te19shimazu@g.kumamoto-nct.ac.jp>
2024-02-14 10:08:53 +0900

    aboutをhomeに

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,17 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Login from './about/Login.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-	<meta name="google-site-verification" content="mLMbMFRzancQaJM0i8r5acdL8YHflcE34EVZ1gr9Khg" />
+	<title>About</title>
+	<meta name="description" content="About this app" />
 </svelte:head>
 
-<section>
-	<h1>Login With Google.</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div>
+	<h1>Userページ</h1>
+	<Login />
+	<div class="blue-button">
+	<a href="/mypage">mypage</a>
+	</div>
+</div>
+<style></style>
